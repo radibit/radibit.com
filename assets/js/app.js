1e138ab73f2f842599b7d4422489dadfc3c9650b
radibit <radimir.bitsov@gmail.com>
2017-01-08 20:30:12 +0100

    ✨ create front page design

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,30 @@
 (function() {
-  //// 1. Define Space and Form
-  var colors = {
-    a1: "#ff2d5d", a2: "#42dc8e", a3: "#2e43eb", a4: "#ffe359",
-    b1: "#96bfed", b2: "#f5ead6", b3: "#f1f3f7", b4: "#e2e6ef"
-  };
-  var space = new Pt.CanvasSpace("space-dots").setup( {bgcolor: "#fff"} );
+  // 1. Define Config
+  var config = {
+    colors: {
+      a1: '#e65b2f', a2: '#f5ead4', a3: '#5fb287', a4: '#5cb2d3',
+      b1: '#ffffff', b2: '#002d37'
+    },
+    ptsNum: 500
+  }
+
+  // 1. Define Space and Form
+  var space = new Pt.CanvasSpace('space-dots').setup( {bgcolor: config.colors.b2} );
   var form = new Pt.Form( space );
 
-
-  //// 2. Create Elements
-  var mouse = new Pt.Circle( space.size.$divide(2) ).setRadius(200);
+  // 2. Create Elements
+  var mouse = new Pt.Circle( space.size.x / 2, 60 ).setRadius(200);
   var pts = [];
-  for (var i=0; i<500; i++) {
+  for (var i = 0; i < config.ptsNum; i++) {
     pts.push( space.size.$multiply( Math.random(), Math.random()) );
   }
 
-  //// 3. Visualize, Animate, Interact
+  // 3. Visualize, Animate, Interact
   space.add({
     animate: function(time, fps, context) {
 
       // change mouse radius
-      mouse.setRadius( mouse.y/space.size.y * 50 + 60 );
+      mouse.setRadius( mouse.y/space.size.y * 100 + 60 );
       form.stroke(false);
 
       // go through each point
@@ -32,12 +36,12 @@
         // if intersecting with mouse circle, change its size and scale from mouse anchor point
         if (mouse.intersectPoint( pts[i]) ) {
           var dist =  (mouse.radius - pts[i].distance( mouse )) / mouse.radius;
-          size = dist * 20;
-          form.fill( colors["a"+(i%4+1)] );
+          size = dist * 15;
+          form.fill( config.colors['a'+(i%4+1)] );
           _p.scale2D( 1+dist, 1+dist, mouse );
 
         } else {
-          form.fill( "#222" );
+          form.fill( config.colors.b1 );
         }
 
         // draw points
@@ -46,13 +50,22 @@
     },
 
     onMouseAction: function(type, x, y, evt) {
-      if (type=="move") {
+      if (type=='move') {
         mouse.set(x,y);
       }
     },
 
     onTouchAction: function(type, x, y, evt) {
       this.onMouseAction( type, x, y );
+    },
+
+    onSpaceResize: function(x, y, evt) {
+      config.ptsNum = x > 500 ? 500 : ~~(x / 2) + 25;
+
+      pts = [];
+      for (var i = 0; i < config.ptsNum; i++) {
+        pts.push( space.size.$multiply( Math.random(), Math.random()) );
+      }
     }
   });
 
