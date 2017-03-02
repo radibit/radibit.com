18a443279a992b55be5f986ca5c432f2e5973bce
radibit <radimir.bitsov@gmail.com>
2017-03-02 08:37:12 +0100

    🔧add eslint, update js files

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,5 @@
-(function() {
+/* eslint-env browser */
+(function(Pt) {
   // 1. Define Config
   var config = {
     colors: {
@@ -6,72 +7,76 @@
       b1: '#ffffff', b2: '#002d37'
     },
     ptsNum: 500
-  }
+  };
 
   // 1. Define Space and Form
-  var space = new Pt.CanvasSpace('space-dots').setup( {bgcolor: config.colors.b2} );
-  var form = new Pt.Form( space );
+  var space = new Pt.CanvasSpace('space-dots').setup({
+    bgcolor: config.colors.b2
+  });
+  var form = new Pt.Form(space);
 
   // 2. Create Elements
-  var mouse = new Pt.Circle( space.size.x / 2, 60 ).setRadius(200);
+  var mouse = new Pt.Circle(space.size.x / 2, 60).setRadius(200);
   var pts = [];
   for (var i = 0; i < config.ptsNum; i++) {
-    pts.push( space.size.$multiply( Math.random(), Math.random()) );
+    pts.push(space.size.$multiply(Math.random(), Math.random()));
   }
 
   // 3. Visualize, Animate, Interact
   space.add({
-    animate: function(time, fps, context) {
+    // Arguments: time, fps, context
+    animate: function() {
 
       // change mouse radius
-      mouse.setRadius( mouse.y/space.size.y * 100 + 60 );
+      mouse.setRadius(mouse.y / space.size.y * 100 + 60);
       form.stroke(false);
 
       // go through each point
-      for (var i=0; i<pts.length; i++) {
+      for (var j = 0; j < pts.length; j++) {
 
         var size = 0.5;
-        var _p = pts[i].clone();
+        var _p = pts[j].clone();
 
         // if intersecting with mouse circle, change its size and scale from mouse anchor point
-        if (mouse.intersectPoint( pts[i]) ) {
-          var dist =  (mouse.radius - pts[i].distance( mouse )) / mouse.radius;
+        if (mouse.intersectPoint(pts[j])) {
+          var dist = (mouse.radius - pts[j].distance(mouse)) / mouse.radius;
           size = dist * 15;
-          form.fill( config.colors['a'+(i%4+1)] );
-          _p.scale2D( 1+dist, 1+dist, mouse );
-
+          form.fill(config.colors['a' + (j % 4 + 1)]);
+          _p.scale2D(1 + dist, 1 + dist, mouse);
         } else {
-          form.fill( config.colors.b1 );
+          form.fill(config.colors.b1);
         }
 
         // draw points
-        form.point( _p, size, true );
+        form.point(_p, size, true);
       }
     },
 
-    onMouseAction: function(type, x, y, evt) {
-      if (type=='move') {
-        mouse.set(x,y);
+    // Arguments: type, x, y, evt
+    onMouseAction: function(type, x, y) {
+      if (type === 'move') {
+        mouse.set(x, y);
       }
     },
 
-    onTouchAction: function(type, x, y, evt) {
-      this.onMouseAction( type, x, y );
+    // Arguments: type, x, y, evt
+    onTouchAction: function(type, x, y) {
+      this.onMouseAction(type, x, y);
     },
 
-    onSpaceResize: function(x, y, evt) {
+    // Arguments: x, y, evt
+    onSpaceResize: function(x) {
       config.ptsNum = x > 500 ? 500 : ~~(x / 2) + 25;
 
       pts = [];
-      for (var i = 0; i < config.ptsNum; i++) {
-        pts.push( space.size.$multiply( Math.random(), Math.random()) );
+      for (var k = 0; k < config.ptsNum; k++) {
+        pts.push(space.size.$multiply(Math.random(), Math.random()));
       }
     }
   });
-
 
   // 4. Start playing
   space.bindMouse();
   space.bindTouch();
   space.play();
-}());
+}(window.Pt));
--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,66 @@
-const child        = require( 'child_process' );
-const browserSync  = require( 'browser-sync' ).create();
-const gulp         = require( 'gulp' );
-const concat       = require( 'gulp-concat' );
-const gutil        = require( 'gulp-util' );
-const sass         = require( 'gulp-sass' );
-const cssnano      = require( 'gulp-cssnano' );
-const autoprefixer = require( 'gulp-autoprefixer' );
+const child = require('child_process');
+const browserSync = require('browser-sync').create();
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const gutil = require('gulp-util');
+const sass = require('gulp-sass');
+const cssnano = require('gulp-cssnano');
+const autoprefixer = require('gulp-autoprefixer');
 
-const siteRoot  = '_site';
+const siteRoot = '_site';
 const scssFiles = '_sass/*.scss';
 
 /**
  * Style compile and concatenate task
  */
-gulp.task( 'css', () => {
-  return gulp.src( '_sass/main.scss' )
-    .pipe( sass() )
-    .pipe( concat( 'main.css' ) )
-    .pipe( autoprefixer( {
-      browsers : [ 'last 2 versions' ],
-			cascade  : false
-    } ) )
-    .pipe( cssnano( {
-        discardComments : {
-          removeAll : true
-        }
-      } ) )
-    .pipe( gulp.dest( './assets/css' ) );
-} );
+gulp.task('css', () => {
+  return gulp.src('_sass/main.scss')
+    .pipe(sass())
+    .pipe(concat('main.css'))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(cssnano({
+      discardComments: {
+        removeAll: true
+      }
+    }))
+    .pipe(gulp.dest('./assets/css'));
+});
 
 /**
  * Run Jekyll build task as child process
  */
-gulp.task( 'jekyll', () => {
-  const jekyll = child.spawn( 'jekyll', [ 'build', '--watch' ] );
+gulp.task('jekyll', () => {
+  const jekyll = child.spawn('jekyll', [ 'build', '--watch' ]);
 
-  const jekyllLogger = ( buffer ) => {
+  const jekyllLogger = (buffer) => {
     buffer.toString()
-      .split( /\n/ )
-      .forEach( ( message ) => gutil.log( 'Jekyll: ' + message ) );
+      .split(/\n/)
+      .forEach((message) => gutil.log('Jekyll: ' + message));
   };
 
-  jekyll.stdout.on( 'data', jekyllLogger );
-  jekyll.stderr.on( 'data', jekyllLogger );
-} );
+  jekyll.stdout.on('data', jekyllLogger);
+  jekyll.stderr.on('data', jekyllLogger);
+});
 
 /**
  * Live reload task
  */
-gulp.task( 'serve', () => {
-  browserSync.init( {
-    files  : [ siteRoot + '/**' ],
-    port   : 4000,
-    server : {
-      baseDir : siteRoot
+gulp.task('serve', () => {
+  browserSync.init({
+    files: [ siteRoot + '/**' ],
+    port: 4000,
+    server: {
+      baseDir: siteRoot
     }
-  } );
+  });
 
-  gulp.watch( scssFiles, [ 'css' ] );
-} );
+  gulp.watch(scssFiles, [ 'css' ]);
+});
 
 /**
  * Watch task
  */
-gulp.task( 'watch', [ 'css', 'jekyll', 'serve' ] );
+gulp.task('watch', [ 'css', 'jekyll', 'serve' ]);
