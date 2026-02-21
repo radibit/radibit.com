96a7aeb9d3968744ce5cae62f5f79d9608ebf362
radibit <radimir.bitsov@gmail.com>
2026-02-21 20:52:09 +0200

    ⚡  prevent render blocking CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    inlineStylesheets: 'always',
+  },
   vite: { plugins: [tailwindcss()] },
 });
